7c8b8d72ef6282f2392c5fb49195f1bad3b8a5b5
Thu Thuy <61125935+mtthuy22@users.noreply.github.com>
2024-02-01 18:57:45 +0100

    feat:cart checkout

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Cart.css";
 
 function Cart({ cart, removeFromCart }) {
+  const [checkOut, setCheckOut] = useState(false);
+
+  function toCheckOut() {
+    setCheckOut(true);
+  }
+
   function totalPrice() {
     return cart.reduce((total, item) => total + item.price, 0).toFixed(2);
   }
   return (
     <>
       <div className="cart bg-secondary text-white mb-5 mx-5 mt-2 py-3 px-3">
-        {cart.length ? (
-          <p className="text-center">Currently in cart:</p>
+        {checkOut ? (
+          <p className="text-center">Your order summary:</p>
         ) : (
-          <p className="text-center">Cart is empty</p>
+          <p className="text-center">
+            {cart.length ? "Currently in cart" : "Your cart is empty"}
+          </p>
         )}
         <ul className="list-group">
           {cart.map((item) => (
@@ -19,29 +27,47 @@ function Cart({ cart, removeFromCart }) {
               <span>
                 {item.name} - {item.price} EUR
               </span>
-              <button
-                type="button"
-                onClick={() => removeFromCart(item)}
-                className="btn btn-danger btn-sm ms-3"
-              >
-                Remove
-              </button>
+
+              {!checkOut && (
+                <button
+                  type="button"
+                  onClick={() => removeFromCart(item)}
+                  className="btn btn-danger btn-sm ms-3"
+                >
+                  Remove
+                </button>
+              )}
             </li>
           ))}
         </ul>
-        {cart.length ? (
+        {cart.length > 0 && (
           <div className="text-end">
             <p className="text-uppercase fw-bold">
-              {" "}
-              <span>total</span>: {totalPrice()} EUR{" "}
+              <span>total</span>: {totalPrice()} EUR
             </p>
-            <button className="btn btn-dark">Check-out</button>
+
+            {checkOut ? (
+              <div>
+                <label htmlFor="email" className="form-label">
+                 Send order to: 
+                </label>
+                <input
+                  type="email"
+                  className="form-control w-25"
+                  id="email"
+                  placeholder="name@example.com"
+                  required
+                />
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </div>
+            ) : (
+              <button onClick={() => toCheckOut()} className="btn btn-dark">
+                Check-out
+              </button>
+            )}
           </div>
-        ) : (
-          <p className="d-none">
-            {" "}
-            <span>total</span>: {totalPrice()} EUR{" "}
-          </p>
         )}
       </div>
     </>
