399c741a099d0f94d95a4d5d35faf16730bd6704
Thu Thuy <61125935+mtthuy22@users.noreply.github.com>
2024-02-16 11:23:28 +0100

    feat:validation message not immediate

--- src/Components/CheckoutForm.jsx
+++ src/Components/CheckoutForm.jsx
@@ -3,44 +3,50 @@ import { useState } from "react";
 
 function CheckoutForm({ toComplete }) {
   const [email, setEmail] = useState("");
-  const [validEmail, setIsValidEmail] = useState(null);
-
+  const [validEmail, setIsValidEmail] = useState("");
+  const [displayValidation, setDisplayValidation] = useState(false);
   let emailRegex = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
 
-  function checkEmail(e) {
+  function inputEmail(e) {
     let input = e.target.value;
     setEmail(input);
-    //console.log(emailRegex.test(input))
-    setIsValidEmail(input === "" ? null : emailRegex.test(input));
+    setIsValidEmail(emailRegex.test(input));
   }
 
-  function handleSubmit() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    setDisplayValidation(true);
     validEmail && toComplete();
   }
 
   return (
     <>
-      <form onSubmit={() => handleSubmit()}>
+      <form onSubmit={(e) => handleSubmit(e)} noValidate>
         <label htmlFor="email" className="form-label">
           Send order to:
         </label>
         <input
           type="email"
           className={`form-control w-25 ${
-            validEmail ? "is-valid" : validEmail === false ? "is-invalid" : ""
+            displayValidation ? (validEmail ? "is-valid" : "is-invalid") : ""
           }`}
           id="email"
           value={email}
           placeholder="name@example.com"
-          onChange={(e) => checkEmail(e)}
+          onChange={(e) => inputEmail(e)}
           required
         />
-        {validEmail ? (
-          <div className="valid-feedback fw-bold text-start">Looks good!</div>
+
+        {displayValidation ? (
+          validEmail ? (
+            <div className="valid-feedback fw-bold text-start">Looks good!</div>
+          ) : (
+            <div className="invalid-feedback fw-bold text-start">
+              Please enter a valid email.
+            </div>
+          )
         ) : (
-          <div className="invalid-feedback fw-bold text-start">
-            Please enter a valid email.
-          </div>
+          ""
         )}
 
         <button type="submit" className="btn btn-primary">
